0f11afc8ad4fbc0199ffb229129291ee8ec7dcfc
lucas404x <joselucas0303@gmail.com>
2019-11-15 16:13:37 -0300

    correcoes no arquivo repositorios

--- models/repositories.py
+++ models/repositories.py
@@ -4,9 +4,7 @@ from tkinter.filedialog import askdirectory
 import os
 
 class Repositories:
-
-    options = {}
-
+    
     @staticmethod
     def extract_repositories(beatiful_soup_object):
         repositories_html_structure = beatiful_soup_object.find(attrs = {'id':'user-repositories-list'}).ul
@@ -19,7 +17,7 @@ class Repositories:
     def get_repositories(repositories):
         print()
         for index, repository in enumerate(repositories):
-            print(f'{index + 1} - {repository.name}: https://github.com{repository.url}')
+            print(f'{index + 1} - {repository.name}: {repository.url}')
     
     @staticmethod
     def clone_repository(repository):
